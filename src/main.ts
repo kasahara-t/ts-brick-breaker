6c347e4341ea906ce2856133cb021be2a3230842
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-02-16 23:34:31 +0900

    feat: ✨ ゲームオーバーの実装

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,4 @@
-import "phaser";
+import Game from "./game";
 import "./global.scss";
-import Example from "./scenes/Example";
 
-const isDebug = import.meta.env.MODE === "development";
-
-const config: Phaser.Types.Core.GameConfig = {
-	width: 480,
-	height: 320,
-	type: Phaser.AUTO,
-	scene: [Example],
-	scale: {
-		mode: Phaser.Scale.FIT, // SHOW_ALLに相当するモード
-		autoCenter: Phaser.Scale.CENTER_BOTH, // ゲームを画面の中央に配置
-	},
-	physics: {
-		default: "arcade",
-		arcade: {
-			gravity: { y: 0 },
-			debug: isDebug,
-		},
-	},
-	backgroundColor: "#eee",
-};
-
-const game = new Phaser.Game(config);
+const game = new Game();
--- src/scenes/Example.ts
+++ src/scenes/Example.ts
@@ -26,32 +26,54 @@ export default class Example extends Phaser.Scene {
 
 	update(): void {
 		this.updatePaddlePosition();
+		this.handleBallPaddleCollision();
 	}
 
 	private createBall(): void {
 		const { width, height } = this.scale;
 		this.ball = this.physics.add
 			.sprite(width * 0.5, height - 25, "ball")
-			.setCollideWorldBounds(true)
+			.setOrigin(0.5)
+			.setCollideWorldBounds(true, 1, 1, true)
 			.setBounce(1, 1)
 			.setVelocity(150, -150);
+
+		this.physics.world.addListener("worldbounds", this.handleOutOfBounds);
 	}
 
 	private createPaddle(): void {
 		const { width, height } = this.scale;
 		this.paddle = this.physics.add
 			.sprite(width * 0.5, height - 5, "paddle")
+			.setOrigin(0.5, 1)
 			.setImmovable(true);
-		this.paddle.setOrigin(0.5, 1);
 	}
 
 	private updatePaddlePosition(): void {
-		if (!this.ball || !this.paddle) return;
-
-		const pointer = this.input.activePointer;
-		if (!pointer.isDown) {
-			// パドルをマウスカーソルまたはタッチ位置に追従させる
-			this.paddle.x = pointer.x;
+		if (this.paddle) {
+			this.paddle.setX(this.input.x);
 		}
+	}
+
+	private handleBallPaddleCollision(): void {
+		if (this.ball && this.paddle) {
+			this.physics.collide(this.ball, this.paddle);
+		}
+	}
+
+	private handleOutOfBounds(
+		body: Phaser.Physics.Arcade.Body,
+		up: boolean,
+		down: boolean,
+	): void {
+		if (down) {
+			alert("Game Over");
+			location.reload();
+		}
+	}
+
+	// シーンのシャットダウン時にイベントリスナーをクリーンアップ
+	public shutdown(): void {
+		this.physics.world.removeListener("worldbounds", this.handleOutOfBounds);
 	}
 }
