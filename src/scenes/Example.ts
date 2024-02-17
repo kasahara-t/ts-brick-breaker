21e468352a05e0c83fa0036b2257dc8b97292a3d
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-02-17 12:20:57 +0900

    feat: ✨ 残機を追加

--- src/scenes/Example.ts
+++ src/scenes/Example.ts
@@ -14,10 +14,20 @@ const brickInfo = {
 	padding: 10,
 };
 
+const textStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+	font: "18px Arial",
+	color: "#0095DD",
+};
+
 export default class Example extends Phaser.Scene {
 	private ball?: Phaser.Physics.Arcade.Sprite;
 	private paddle?: Phaser.Physics.Arcade.Sprite;
 	private bricks?: Phaser.Physics.Arcade.Group;
+	private score = 0;
+	private scoreText?: Phaser.GameObjects.Text;
+	private life = 3;
+	private lifeText?: Phaser.GameObjects.Text;
+	private lifeLostText?: Phaser.GameObjects.Text;
 
 	constructor() {
 		super("Example");
@@ -42,6 +52,8 @@ export default class Example extends Phaser.Scene {
 		this.createBall();
 		this.createPaddle();
 		this.createBricks();
+		this.createScoreText();
+		this.createLifeText();
 	}
 
 	public update(): void {
@@ -59,7 +71,7 @@ export default class Example extends Phaser.Scene {
 			.setBounce(1)
 			.setVelocity(150, -150);
 
-		this.physics.world.addListener("worldbounds", this.handleOutOfBounds);
+		this.physics.world.addListener("worldbounds", this.handleOutOfBounds, this);
 	}
 
 	private createPaddle(): void {
@@ -86,21 +98,56 @@ export default class Example extends Phaser.Scene {
 		}
 	}
 
+	private createScoreText(): void {
+		this.scoreText = this.add.text(5, 5, "Points: 0", textStyle);
+	}
+
+	private createLifeText(): void {
+		this.lifeText = this.add
+			.text(this.scale.width - 5, 5, `Lives: ${this.life}`, textStyle)
+			.setOrigin(1, 0);
+		this.lifeLostText = this.add
+			.text(
+				this.scale.width * 0.5,
+				this.scale.height * 0.5,
+				"Life lost, click to continue",
+				textStyle,
+			)
+			.setOrigin(0.5)
+			.setVisible(false);
+	}
+
 	private updatePaddlePosition(): void {
 		if (!this.paddle) return;
+		if (this.lifeLostText?.visible) return;
+
 		this.paddle.setX(this.input.x);
 	}
 
 	private handleBallPaddleCollision(): void {
 		if (!this.ball || !this.paddle || !this.bricks) return;
+
 		this.physics.collide(this.ball, this.paddle);
 	}
 
 	private handleBrickBallCollision(): void {
 		if (!this.ball || !this.paddle || !this.bricks) return;
-		this.physics.collide(this.ball, this.bricks, (_, brick) => {
+
+		const ballHitBrick: Phaser.Types.Physics.Arcade.ArcadePhysicsCallback = (
+			ball,
+			brick,
+		) => {
 			brick.destroy(true);
-		});
+			this.score += 10;
+			this.scoreText?.setText(`Points: ${this.score}`);
+
+			if (this.bricks?.countActive() === 0) {
+				alert("You won the game, congratulations!");
+				location.reload();
+			}
+		};
+
+		this.physics.collide(this.ball, this.bricks, ballHitBrick);
 	}
 
 	private handleOutOfBounds(
@@ -108,14 +155,34 @@ export default class Example extends Phaser.Scene {
 		up: boolean,
 		down: boolean,
 	): void {
-		if (down) {
+		if (!down) return;
+		if (!this.ball || !this.paddle) return;
+		if (!this.lifeText || !this.lifeLostText) return;
+
+		if (--this.life === 0) {
 			alert("Game Over");
 			location.reload();
 		}
+
+		this.lifeText.setText(`Lives: ${this.life}`);
+		this.lifeLostText.setVisible(true);
+
+		const { width, height } = this.scale;
+		this.ball.setPosition(width * 0.5, height - 25).setVelocity(0, 0);
+		this.paddle.setPosition(width * 0.5, height - 5);
+
+		this.input.on("pointerdown", () => {
+			this.lifeLostText?.setVisible(false);
+			this.ball?.setVelocity(150, -150);
+		});
 	}
 
 	// シーンのシャットダウン時にイベントリスナーをクリーンアップ
 	public shutdown(): void {
-		this.physics.world.removeListener("worldbounds", this.handleOutOfBounds);
+		this.physics.world.removeListener(
+			"worldbounds",
+			this.handleOutOfBounds,
+			this,
+		);
 	}
 }
